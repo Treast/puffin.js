e6e312f1778a37762ee0e7bd8cefa3da3a7c2f46
Vincent Riva <vincent.riva@outlook.fr>
2020-10-17 00:08:53 +0200

    Change Rollup config, remove deps, update README.md

--- rollup.config.js
+++ rollup.config.js
@@ -2,24 +2,31 @@ import scss from 'rollup-plugin-scss';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
-// import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
+// import pkg from './package.json';
 
 export default [
   {
     input: 'src/Puffin.js',
-    output: {
-      name: 'main',
-      file: 'dist/puffin.min.js',
-      format: 'iife',
-      compact: true,
-    },
+    output: [
+      {
+        name: 'Puffin',
+        file: 'dist/puffin.min.js',
+        format: 'iife',
+        compact: true,
+      },
+    ],
     plugins: [
-      babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+      }),
       resolve(),
       commonjs(),
       scss({
         outputStyle: 'compressed',
       }),
+      terser(),
     ],
   },
 ];
